3f2b6521b9316f3a4971f16e6f19878d7128baae
Alexander Jank <himself@alexanderjank.de>
2016-12-26 13:14:38 +0100

    Added new methods for DER formatted keys and HPKP hash generation (#56)
    * Added method for PEM to DER key conversion
    * Added new methods to create a PublicKey instance from a DER formatted key and for creating HPKP hashes

--- src/Ssl/PublicKey.php
+++ src/Ssl/PublicKey.php
@@ -12,6 +12,7 @@
 namespace AcmePhp\Ssl;
 
 use AcmePhp\Ssl\Exception\KeyFormatException;
+use Webmozart\Assert\Assert;
 
 /**
  * Represent a SSL Public key.
@@ -26,9 +27,35 @@ class PublicKey extends Key
     public function getResource()
     {
         if (!$resource = openssl_pkey_get_public($this->keyPEM)) {
-            throw new KeyFormatException(sprintf('Fail to convert key into resource: %s', openssl_error_string()));
+            throw new KeyFormatException(sprintf('Failed to convert key into resource: %s', openssl_error_string()));
         }
 
         return $resource;
+    }
+
+    /**
+     * @param $keyDER
+     *
+     * @return PublicKey
+     */
+    public static function fromDER($keyDER)
+    {
+        Assert::stringNotEmpty($keyDER, __METHOD__.'::$keyDER should be a non-empty string. Got %s');
+
+        $der = base64_encode($keyDER);
+        $lines = str_split($der, 65);
+        array_unshift($lines, '-----BEGIN PUBLIC KEY-----');
+        $lines[] = '-----END PUBLIC KEY-----';
+        $lines[] = '';
+
+        return new self(implode("\n", $lines));
+    }
+
+    /**
+     * @return string
+     */
+    public function getHPKP()
+    {
+        return base64_encode(hash('sha256', $this->getDER(), true));
     }
 }
