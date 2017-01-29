0b4ea8b586e028ba4ea7f7411adfd2ec7b77c095
Titouan Galopin <galopintitouan@gmail.com>
2017-01-29 16:56:18 +0100

    Bump to dev

--- src/Cli/Application.php
+++ src/Cli/Application.php
@@ -28,7 +28,7 @@ use Webmozart\PathUtil\Path;
  */
 class Application extends BaseApplication
 {
-    const VERSION = '1.0.0-beta4';
+    const VERSION = '1.0.0-dev';
 
     /**
      * {@inheritdoc}
