034b6a25b9541468700cfa84b670fdcf7f34e2d0
Jérémy Derussé <jeremy@derusse.com>
2018-12-03 08:57:49 +0100

    Fix exception constructor

--- src/Core/Exception/AcmeDnsResolutionException.php
+++ src/Core/Exception/AcmeDnsResolutionException.php
@@ -18,6 +18,6 @@ class AcmeDnsResolutionException extends AcmeCoreException
 {
     public function __construct($message, \Exception $previous = null)
     {
-        parent::__construct(null === $message ? 'An exception was thrown during resolution of DNS' : $message, $previous);
+        parent::__construct(null === $message ? 'An exception was thrown during resolution of DNS' : $message, 0, $previous);
     }
 }
