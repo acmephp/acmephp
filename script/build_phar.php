856526d53e04aa14fa0321ecc7e26873c5d44055
xiaohui.lam <xiaohui.lam@icloud.com>
2019-07-31 16:08:14 +0800

    fix phar build

--- script/build_phar.php
+++ script/build_phar.php
@@ -1,6 +1,9 @@
 <?php
 
 $phar = new Phar('/tmp/acmephp.phar');
+$phar->startBuffering();
 $phar->buildFromDirectory(__DIR__ . '/../');
 $phar->compressFiles(Phar::GZ);
 $phar->setDefaultStub('bin/acme');
+$phar->setSignatureAlgorithm(Phar::SHA256);
+$phar->stopBuffering();
