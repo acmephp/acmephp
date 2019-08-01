505d4c2d60229e89103f49e396b792cefc5c2a86
xiaohui.lam <xiaohui.lam@icloud.com>
2019-08-01 18:00:30 +0800

    stash

--- src/Cli/Action/InstallAliyunCdnAction.php
+++ src/Cli/Action/InstallAliyunCdnAction.php
@@ -13,8 +13,7 @@ namespace AcmePhp\Cli\Action;
 
 use AcmePhp\Ssl\CertificateResponse;
 use AlibabaCloud\Client\AlibabaCloud;
-use AlibabaCloud\Client\Exception\ClientException;
-use AlibabaCloud\Client\Exception\ServerException;
+use AlibabaCloud\Cdn\Cdn;
 
 /**
  * Action to install certificate in an Aliyun Waf.
@@ -38,23 +37,14 @@ class InstallAliyunCdnAction extends AbstractAction
 
         $key = $response->getCertificateRequest()->getKeyPair()->getPrivateKey()->getPEM();
 
-        try {
-            AlibabaCloud::accessKeyClient($config['accessKeyId'], $config['accessKeySecret'])->regionId('cn-hangzhou')->asDefaultClient();
-            $response = WafOpenapi::v20161111()->upgradeInstance()
-                ->host('wafopenapi.cn-hangzhou.aliyuncs.com')
-                ->action('CreateCertAndKey')
-                ->setProtocol('https')
-                ->version('2018-01-17')
-                ->withCert($cert)
-                ->withKey($key)
-                ->withDomain($config['domain'])
-                ->withHttpsCertName($config['domain'])
-                ->withInstanceId($config['instanceId'])
-                ->request();
-        } catch (ServerException $e) {
-            throw $e;
-        } catch (ClientException $e) {
-            throw $e;
-        }
+        AlibabaCloud::accessKeyClient($config['accessKeyId'], $config['accessKeySecret'])->regionId('cn-hangzhou')->asDefaultClient();
+        Cdn::v20180510()->setDomainServerCertificate()
+            ->withDomainName($config['domain'])
+            ->withCertName($config['domain'])
+            ->withCertType('upload')
+            ->withForceSet(1)
+            ->withServerCertificate($cert)
+            ->withPrivateKey($key)
+            ->withServerCertificateStatus('on');
     }
 }
