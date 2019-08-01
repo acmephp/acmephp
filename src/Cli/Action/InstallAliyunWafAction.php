dcd1ad771389fa738fd759cddebf8cb480d0af3c
xiaohui.lam <xiaohui.lam@icloud.com>
2019-08-01 21:18:23 +0800

    fixbug

--- src/Cli/Action/InstallAliyunWafAction.php
+++ src/Cli/Action/InstallAliyunWafAction.php
@@ -28,6 +28,8 @@ class InstallAliyunWafAction extends AbstractAction
     public function handle($config, CertificateResponse $response)
     {
         $issuerChain = [];
+        $issuerChain[] = $response->getCertificate()->getPEM();
+
         $issuerCertificate = $response->getCertificate()->getIssuerCertificate();
         while (null !== $issuerCertificate) {
             $issuerChain[] = $issuerCertificate->getPEM();
