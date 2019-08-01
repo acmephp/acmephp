6d17d7b91a3d6e890487279c22cb46fb65e0c8b2
xiaohui.lam <xiaohui.lam@icloud.com>
2019-08-01 18:30:10 +0800

    remove useless sh

--- script/build_phar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$phar = new Phar('/tmp/acmephp.phar');
-$phar->startBuffering();
-$phar->buildFromDirectory(__DIR__ . '/../');
-$phar->compressFiles(Phar::GZ);
-$phar->setDefaultStub('bin/acme');
-$phar->setSignatureAlgorithm(Phar::SHA256);
-$phar->stopBuffering();
