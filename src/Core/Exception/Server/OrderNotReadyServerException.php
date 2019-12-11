c592b8500f0123eb4f7077763b4f9faeac132f0d
Paul Adams <paul.adams@thecoderszone.com>
2019-12-11 22:34:43 +0000

    added orderNotReady error type

---
+++ src/Core/Exception/Server/OrderNotReadyServerException.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the Acme PHP project.
+ *
+ * (c) Titouan Galopin <galopintitouan@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AcmePhp\Core\Exception\Server;
+
+use AcmePhp\Core\Exception\AcmeCoreServerException;
+use Psr\Http\Message\RequestInterface;
+
+class OrderNotReadyServerException extends AcmeCoreServerException
+{
+    public function __construct(RequestInterface $request, $detail, \Exception $previous = null)
+    {
+        parent::__construct(
+            $request,
+            '[orderNotReady] Order could not be finalized: '.$detail,
+            $previous
+        );
+    }
+}
