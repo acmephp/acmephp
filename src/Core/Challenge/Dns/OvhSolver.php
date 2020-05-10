5f5ace703244c056e287f50ec942b1e9e982b94b
Nicolas SCHREVEL <nicolas.schrevel@l3ia.fr>
2020-05-10 13:45:13 +0200

    php_cs fixer

--- src/Core/Challenge/Dns/OvhSolver.php
+++ src/Core/Challenge/Dns/OvhSolver.php
@@ -15,7 +15,6 @@ use AcmePhp\Core\Challenge\ConfigurableServiceInterface;
 use AcmePhp\Core\Challenge\MultipleChallengesSolverInterface;
 use AcmePhp\Core\Protocol\AuthorizationChallenge;
 use GuzzleHttp\Client;
-use GuzzleHttp\ClientException;
 use GuzzleHttp\ClientInterface;
 use Ovh\Api;
 use Psr\Log\LoggerAwareTrait;
@@ -34,37 +33,37 @@ class OvhSolver implements MultipleChallengesSolverInterface, ConfigurableServic
      * @var DnsDataExtractor
      */
     private $extractor;
-    
+
     /**
      * @var ClientInterface
      */
     private $client;
-    
+
     /**
      * @var array
      */
     private $cacheZones;
-    
+
     /**
      * @var string
      */
     private $appKey;
-    
+
     /**
      * @var string
      */
     private $appSecret;
-    
+
     /**
      * @var string
      */
     private $endPoint;
-    
+
     /**
      * @var string
      */
     private $consumerKey;
-    
+
     /**
      * @param DnsDataExtractor $extractor
      * @param ClientInterface  $client
@@ -73,11 +72,11 @@ class OvhSolver implements MultipleChallengesSolverInterface, ConfigurableServic
         DnsDataExtractor $extractor = null,
         ClientInterface $client = null
         ) {
-            $this->extractor = null === $extractor ? new DnsDataExtractor() : $extractor;
-            $this->client = null === $client ? new Client() : $client;
-            $this->logger = new NullLogger();
+        $this->extractor = null === $extractor ? new DnsDataExtractor() : $extractor;
+        $this->client = null === $client ? new Client() : $client;
+        $this->logger = new NullLogger();
     }
-    
+
     /**
      * Configure the service with a set of configuration.
      */
@@ -88,7 +87,7 @@ class OvhSolver implements MultipleChallengesSolverInterface, ConfigurableServic
         $this->endPoint = $config['end_point'];
         $this->consumerKey = $config['consumer_key'];
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -96,7 +95,7 @@ class OvhSolver implements MultipleChallengesSolverInterface, ConfigurableServic
     {
         return 'dns-01' === $authorizationChallenge->getType();
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -104,56 +103,55 @@ class OvhSolver implements MultipleChallengesSolverInterface, ConfigurableServic
     {
         return $this->solveAll([$authorizationChallenge]);
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function solveAll(array $authorizationChallenges)
     {
         Assert::allIsInstanceOf($authorizationChallenges, AuthorizationChallenge::class);
-        
+
         foreach ($authorizationChallenges as $authorizationChallenge) {
-            
-            $this->logger->debug("Create TXT for " . $authorizationChallenge->getDomain());
-            
+            $this->logger->debug('Create TXT for '.$authorizationChallenge->getDomain());
+
             $topLevelDomain = $this->getTopLevelDomain($authorizationChallenge->getDomain());
             $recordName = $this->extractor->getRecordName($authorizationChallenge);
             $recordValue = $this->extractor->getRecordValue($authorizationChallenge);
-            
+
             $subDomain = \str_replace('.'.$topLevelDomain.'.', '', $recordName);
-            
+
             $client = new Client(['http_errors' => false]);
-            
+
             $ovh = new Api($this->appKey,
                 $this->appSecret,
                 $this->endPoint,
                 $this->consumerKey,
                 $client);
 
-            $result = $ovh->post('/domain/zone/' . $topLevelDomain . '/record', array(
+            $result = $ovh->post('/domain/zone/'.$topLevelDomain.'/record', [
             'fieldType' => 'TXT',
             'subDomain' => $subDomain,
             'target' => $recordValue,
             'ttl' => 600,
-            ));
-            
-            if (isset($result['message']) && $result['message'] != ""){
-                $this->logger->error("OVH Exception Post = " . $result['message'] . " " . $topLevelDomain);
-                throw new \Exception("OVH Exception Post = " . $result['message'] . " " . $topLevelDomain);
+            ]);
+
+            if (isset($result['message']) && '' !== $result['message']) {
+                $this->logger->error('OVH Exception Post = '.$result['message'].' '.$topLevelDomain);
+                throw new \Exception('OVH Exception Post = '.$result['message'].' '.$topLevelDomain);
             }
-            
-            $this->logger->info("Refresh zone for " . $authorizationChallenge->getDomain());
-            $result = $ovh->post('/domain/zone/' . $topLevelDomain . '/refresh');
-            
-            if (isset($result['message']) && $result['message'] != ""){
-                $this->logger->error("OVH Exception Refresh= " . $result['message'] . " " . $topLevelDomain);
-                throw new \Exception("OVH Exception Refresh= " . $result['message'] . " " . $topLevelDomain);
+
+            $this->logger->info('Refresh zone for '.$authorizationChallenge->getDomain());
+            $result = $ovh->post('/domain/zone/'.$topLevelDomain.'/refresh');
+
+            if (isset($result['message']) && '' !== $result['message']) {
+                $this->logger->error('OVH Exception Refresh= '.$result['message'].' '.$topLevelDomain);
+                throw new \Exception('OVH Exception Refresh= '.$result['message'].' '.$topLevelDomain);
             }
-            
-            $this->logger->debug("Create done.");
+
+            $this->logger->debug('Create done.');
         }
     }
-    
+
     /**
      * {@inheritdoc}
      */
@@ -161,63 +159,62 @@ class OvhSolver implements MultipleChallengesSolverInterface, ConfigurableServic
     {
         return $this->cleanupAll([$authorizationChallenge]);
     }
-    
+
     /**
      * {@inheritdoc}
      */
     public function cleanupAll(array $authorizationChallenges)
     {
         Assert::allIsInstanceOf($authorizationChallenges, AuthorizationChallenge::class);
-        
+
         foreach ($authorizationChallenges as $authorizationChallenge) {
-            
-            $this->logger->debug("Cleanup record id for " . $authorizationChallenge->getDomain());
-            
+            $this->logger->debug('Cleanup record id for '.$authorizationChallenge->getDomain());
+
             $topLevelDomain = $this->getTopLevelDomain($authorizationChallenge->getDomain());
             $recordName = $this->extractor->getRecordName($authorizationChallenge);
-            
+
             $subDomain = \str_replace('.'.$topLevelDomain.'.', '', $recordName);
-            
+
             $client = new Client(['http_errors' => false]);
-            
+
             $ovh = new Api($this->appKey,
                 $this->appSecret,
                 $this->endPoint,
                 $this->consumerKey,
                 $client);
-            
-            $result = $ovh->get('/domain/zone/' . $topLevelDomain . '/record', array(
+
+            $result = $ovh->get('/domain/zone/'.$topLevelDomain.'/record', [
             'fieldType' => 'TXT', // Filter the value of fieldType property (like) (type: zone.NamedResolutionFieldTypeEnum)
             'subDomain' => $subDomain, // Filter the value of subDomain property (like) (type: string)
-            ));
-            
-            if (isset($result['message']) && $result['message'] != ""){
-                $this->logger->error("OVH Exception Get= " . $result['message'] . " " . $topLevelDomain);
-                throw new \Exception("OVH Exception Get= " . $result['message'] . " " . $topLevelDomain);
+            ]);
+
+            if (isset($result['message']) && '' !== $result['message']) {
+                $this->logger->error('OVH Exception Get= '.$result['message'].' '.$topLevelDomain);
+                throw new \Exception('OVH Exception Get= '.$result['message'].' '.$topLevelDomain);
             }
-            
-            if (count($result) == 1 && isset($result[0])){
+
+            if (1 === \count($result) && isset($result[0])) {
                 $id = $result[0];
-                $this->logger->debug("Delete record id for " . $authorizationChallenge->getDomain() . " " . $id);
-                $result = $ovh->delete('/domain/zone/' . $topLevelDomain .'/record/' .$id);
-                
-                if (isset($result['message']) && $result['message'] != ""){
-                    $this->logger->error("OVH Exception Delete= " . $result['message'] . " " . $topLevelDomain);
-                    throw new \Exception("OVH Exception Delete= " . $result['message'] . " " . $topLevelDomain);
+                $this->logger->debug('Delete record id for '.$authorizationChallenge->getDomain().' '.$id);
+                $result = $ovh->delete('/domain/zone/'.$topLevelDomain.'/record/'.$id);
+
+                if (isset($result['message']) && '' !== $result['message']) {
+                    $this->logger->error('OVH Exception Delete= '.$result['message'].' '.$topLevelDomain);
+                    throw new \Exception('OVH Exception Delete= '.$result['message'].' '.$topLevelDomain);
                 }
-                
-                $this->logger->debug("Refresh zone for " . $authorizationChallenge->getDomain());
-                $result = $ovh->post('/domain/zone/' . $topLevelDomain . '/refresh');
-                if (isset($result['message']) && $result['message'] != ""){
-                    $this->logger->error("OVH Exception Refresh= " . $result['message'] . " " . $topLevelDomain);
-                    throw new \Exception("OVH Exception Refresh= " . $result['message'] . " " . $topLevelDomain);
+
+                $this->logger->debug('Refresh zone for '.$authorizationChallenge->getDomain());
+                $result = $ovh->post('/domain/zone/'.$topLevelDomain.'/refresh');
+                if (isset($result['message']) && '' !== $result['message']) {
+                    $this->logger->error('OVH Exception Refresh= '.$result['message'].' '.$topLevelDomain);
+                    throw new \Exception('OVH Exception Refresh= '.$result['message'].' '.$topLevelDomain);
                 }
-                
-                $this->logger->debug("Cleanup done");
+
+                $this->logger->debug('Cleanup done');
             }
         }
     }
-    
+
     /**
      * @param string $domain
      *
