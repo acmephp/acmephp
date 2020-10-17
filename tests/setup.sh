7d6b566d043ea4de38d1b391fc31ca230ac3af33
Titouan Galopin <galopintitouan@gmail.com>
2020-10-17 17:51:12 +0200

    Fix tests

--- tests/setup.sh
+++ tests/setup.sh
@@ -12,4 +12,4 @@ docker run -d --name acme_server --net host letsencrypt/pebble-challtestsrv pebb
 docker run -d --name acme_pebble --net host -e PEBBLE_VA_NOSLEEP=1 -e PEBBLE_WFE_NONCEREJECT=0 letsencrypt/pebble pebble -dnsserver 127.0.0.1:8053
 
 # Wait for boot to be completed
-docker run --rm --net host martin/wait -c localhost:14000,localhost:80,localhost:8022,localhost:8053,localhost:5002 -t 120
+docker run --rm --net host martin/wait -c localhost:14000,localhost:8022,localhost:8053,localhost:5002 -t 120
